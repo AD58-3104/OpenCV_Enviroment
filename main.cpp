9d2f1899b84e4a93056afc9f5193c542b6cf8d11
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2022-04-29 00:17:40 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,19 +1,14 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
-int
-main(int argc, char *argv[])
+#include <opencv2/opencv.hpp>
+int main(int argc, char *argv[])
 {
-  cv::Mat img = cv::Mat::zeros(500, 500, CV_8UC3);
+    cv::Mat img = cv::Mat::zeros(500, 500, CV_8UC3);
 
-  // Red，太さ3，4近傍連結
-  cv::line(img, cv::Point(100, 100), cv::Point(400, 105), cv::Scalar(0,0,200), 3, 4);  
-  // Green，太さ5，8近傍連結
-  cv::line(img, cv::Point(100, 200), cv::Point(400, 205), cv::Scalar(0,200,0), 5, 8);
-  // Blue，太さ10，アンチエイリアス
-  cv::line(img, cv::Point(100, 300), cv::Point(400, 305), cv::Scalar(200,0,0), 10, CV_AA);
-
-  cv::namedWindow("drawing", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
-  cv::imshow("drawing", img);
-  cv::waitKey(0);
+    cv::circle(img, cv::Point(95, 355), 95, cv::Scalar(255, 0, 0), 5);
+    cv::line(img, cv::Point(100, 100), cv::Point(400, 105), cv::Scalar(0,0,200), 3, 4);
+    
+    cv::namedWindow("drawing");
+    cv::imshow("drawing", img);
+    cv::waitKey(0);
 }
